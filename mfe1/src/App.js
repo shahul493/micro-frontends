1ad1422d78653124528ea3f2f18f28a3bfed4446
Shahul Shaik <shahul493@gmail.com>
2024-07-10 18:56:18 -0400

    inital code base

---
+++ mfe1/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Button from './Button';
+function App() {
+  return (
+    <div>
+      <h1>This is MFE1 app</h1>
+      <Button>
+        {' '}
+        MFE1 Button
+      </Button>
+    </div>
+  );
+}
+
+export default App;
---
+++ mfe2/webpack.prod.js
@@ -0,0 +1,41 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ModuleFederationPlugin from 'webpack/lib/container/ModuleFederationPlugin';
+
+export const mode = 'production';
+export const plugins = [
+  new ModuleFederationPlugin(
+    {
+      name: 'MFE2',
+      filename: 'remoteEntry.js',
+      remotes: {
+        MFE1: 'MFE1@http://localhost:3000/mfe1/dist/remoteEntry.js',
+      },
+    }
+  ),
+  new HtmlWebpackPlugin({
+    template: './public/index.html',
+  }),
+];
+export const module = {
+  rules: [
+    {
+      /* The following line to ask babel
+           to compile any file with extension
+           .js */
+      test: /\.js?$/,
+
+      /* exclude node_modules directory from babel.
+          Babel will not compile any files in this directory*/
+      exclude: /node_modules/,
+
+      // To Use babel Loader
+      loader: 'babel-loader',
+      options: {
+        presets: [
+          '@babel/preset-env' /* to transfer any advansed ES to ES5 */,
+          '@babel/preset-react',
+        ], // to compile react to ES5
+      },
+    },
+  ],
+};
